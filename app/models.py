7a2903eee14ffb91f693510927977e4a53d3269b
opaklsk628 <opaklsk628@gmail.com>
2019-04-23 11:25:48 -0400

    ver1

--- app/models.py
+++ app/models.py
@@ -78,3 +78,84 @@ class Employee(Model):
 
     def __repr__(self):
         return self.full_name
+
+class Products(Model):
+    __tablename__ = 'Products'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False)
+    price = Column(Integer, nullable=False)
+    status = Column(String(3), nullable=False)
+    created_at = Column(Integer, nullable=False)
+    categorie_id = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    supplier_id = Column(Integer, nullable=False)
+    branch_id = Column(Integer, nullable=False)
+    brand_id = Column(Integer, nullable=False)
+
+class Orders(Model):
+    __tablename__ = 'Orders'
+    orders_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    status = Column(String(3), nullable=False)
+    created_at = Column(Integer, nullable=False)
+
+class Order_items(Model):
+    __tablename__ = 'Order_items'
+    orders_id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+class Supplier(Model):
+    __tablename__ = 'Supplier'
+    Supplier_id = Column(Integer, primary_key=True)
+    supplier_name = Column(String(150), nullable=False)
+    created_at = Column(Integer, nullable=False)
+
+class Invoices(Model):
+    __tablename__ = 'Invoices'
+    invoices_number = Column(Integer, primary_key=True)
+    order_id = Column(Integer, nullable=False)
+    invoices_date = Column(Date, default=datetime.date.today(), nullable=True)
+    invoices_details = Column(String(250), nullable=False)
+    payments_id = Column(Integer, nullable=False)
+    delivery_id = Column(Integer, nullable=False)
+
+class Payments(Model):
+    __tablename__ = 'Payments'
+    payments_id = Column(Integer, primary_key=True)
+    invoices_number = Column(Integer, nullable=False)
+    Payment_date = Column(Date, default=datetime.date.today(), nullable=True)
+    payment_amount = Column(Integer, nullable=False)
+
+class Delivery_address(Model):
+    __tablename__ = 'Delivery_address'
+    delivery_id = Column(Integer, primary_key=True)
+    foremane = Column(String(150), nullable=False)
+    address = Column(String(200), nullable=False)
+    postcode = Column(Integer, nullable=False)
+    phone_number = Column(Integer, nullable=False)
+    email = Column(String(100), nullable=False)
+
+class Categories(Model):
+    __tablename__ = 'Categories'
+    categorie_id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(100), nullable=False)
+
+class Branch(Model):
+    __tablename__ = 'Branch'
+    branch_id = Column(Integer, primary_key=True)
+    address = Column(String(200), nullable=False)
+    phone_number = Column(Integer, nullable=False)
+
+class Staff(Model):
+    __tablename__ = 'Staff'
+    Staff_id = Column(Integer, primary_key=True)
+    staff_name = Column(String(50), nullable=False)
+    phone_number = Column(Integer, nullable=False)
+
+class Brands(Model):
+    __tablename__ = 'Brands'
+    brand_id = Column(Integer, primary_key=True)
+    brand_name = Column(String(50), nullable=False)
+    brand_info = Column(String(100), nullable=False)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from flask_appbuilder import ModelView
+from flask_appbuilder import ModelView, has_access
 from flask_appbuilder.fieldwidgets import Select2Widget
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from .models import Employee,Department, Function, EmployeeHistory, Benefit
+from .models import Employee,Department, Function, EmployeeHistory, Benefit, Products, Orders, Order_items, Supplier, Invoices, Payments, Delivery_address, Categories, Branch, Brands
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from app import appbuilder, db
+from flask_appbuilder.baseviews import expose, BaseView
+
 
 
 def department_query():
@@ -46,8 +48,84 @@ class BenefitView(ModelView):
     show_columns = ['name']
     list_columns = ['name']
 
+class ProductsView(ModelView):
+    datamodel = SQLAInterface(Products)
+    list_columns = ['id', 'name', 'price', 'status', 'created_at', 'categorie_id', 'quantity', 'supplier_id', 'branch_id', 'brand_id']
+
+class BrandsView(ModelView):
+    datamodel = SQLAInterface(Brands)
+    list_columns = ['brand_id','brand_name', 'brand_info']
+
+class OrdersView(ModelView):
+    datamodel = SQLAInterface(Orders)
+    list_columns = ['orders_id', 'user_id', 'status', 'created_at']
+
+class OrderitemsView(ModelView):
+    datamodel = SQLAInterface(Order_items)
+    list_columns = ['order_id', 'product_id', 'quantity']
+
+class SupplierView(ModelView):
+    datamodel = SQLAInterface(Supplier)
+    list_columns = ['Supplier_id', 'supplier_name', 'created_at']
+
+class InvoicesView(ModelView):
+    datamodel = SQLAInterface(Invoices)
+    list_columns = ['invoices_number', 'order_id', 'invoices_date']
+
+class PaymentsView(ModelView):
+    datamodel = SQLAInterface(Payments)
+    list_columns = ['payments_id', 'invoices_number', 'Payment_date', 'payment_amount']
+
+class Delivery_addressView(ModelView):
+    datamodel = SQLAInterface(Delivery_address)
+    list_columns = ['delivery_id', 'foremane', 'address', 'postcode', 'phone_number', 'email']
+
+class CategoriesView(ModelView):
+    datamodel = SQLAInterface(Categories)
+    list_columns = ['categorie_id', 'name', 'description']
+
+class BranchView(ModelView):
+    datamodel = SQLAInterface(Branch)
+    list_columns = ['branch_id', 'address', 'phone_number']
+
+class ProductPageView(BaseView):
+    default_view = 'hit_product'
+
+    @expose('/hit_product/')
+    def hit_product(self):
+        param1 = '2019 Hit Product'
+        self.update_redirect()
+        return self.render_template('product.html', param1=param1)
+
+    @expose('/phone/')
+    def phone(self):
+        param1 = 'Phone'
+        self.update_redirect()
+        return self.render_template('product.html', param1=param1)
+
+class PaymentPageView(BaseView):
+    default_view = 'payment'
+
+    @expose('/payment/')
+    def payment(self):
+        param1 = 'Payment'
+        self.update_redirect()
+        return self.render_template('payment.html', param1=param1)
+
+
 
 db.create_all()
+
+""" Page View """
+appbuilder.add_view(ProductPageView, 'Hit Product', category="Product")
+appbuilder.add_link("Phone", href="/productpageview/phone/", category="Product")
+appbuilder.add_view(PaymentPageView, 'payment', category="payment")
+
+
+# Add a link
+appbuilder.add_link("Facebook", href="https://www.facebook.com/BroadwayHK/", icon = "fa-facebook")
+appbuilder.add_link("Promotions", href="https://www.broadwaylifestyle.com/promotions/")
+
 
 appbuilder.add_view_no_menu(EmployeeHistoryView, "EmployeeHistoryView")
 appbuilder.add_view(EmployeeView, "Employees", icon="fa-folder-open-o", category="Company")
@@ -55,3 +133,13 @@ appbuilder.add_separator("Company")
 appbuilder.add_view(DepartmentView, "Departments", icon="fa-folder-open-o", category="Company")
 appbuilder.add_view(FunctionView, "Functions", icon="fa-folder-open-o", category="Company")
 appbuilder.add_view(BenefitView, "Benefits", icon="fa-folder-open-o", category="Company")
+appbuilder.add_view(OrdersView, 'Order', category="Order")
+appbuilder.add_view(OrderitemsView, 'List', category="Order items")
+appbuilder.add_view(ProductsView, 'Product list', category="Control Panel")
+appbuilder.add_view(BrandsView, 'Brands List', category="Control Panel")
+appbuilder.add_view(SupplierView, 'Supplier', category="Control Panel")
+appbuilder.add_view(InvoicesView, 'Invoices', category="Control Panel")
+appbuilder.add_view(PaymentsView, 'Payments', category="Control Panel")
+appbuilder.add_view(Delivery_addressView, 'Delivery_address', category="Control Panel")
+appbuilder.add_view(CategoriesView, 'Categories', category="Control Panel")
+appbuilder.add_view(BranchView, 'Branch', category="Control Panel")
